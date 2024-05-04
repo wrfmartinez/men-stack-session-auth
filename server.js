b4caa7c9f24f65fa4c5d7e58ea840f473260ca95
wrfmartinez <wrmartinez709@gmail.com>
2024-05-03 20:55:32 -0400

    Finish project

---
+++ server.js
@@ -0,0 +1,54 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const session = require('express-session');
+
+// Set the port from environment variable or default to 3000
+const PORT = process.env.PORT || 3000;
+
+const authController = require("./controllers/auth.js");
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride("_method"));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.get('/', async (req, res) => {
+  res.render('index.ejs', {
+    user: req.session.user,
+  });
+})
+
+app.use("/auth", authController);
+
+app.get("/vip-lounge", (req, res) => {
+  if (req.session.user) {
+    res.send(`Welcome to the party ${req.session.user.email}.`);
+  } else {
+    res.send("Sorry, no guests allowed.");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`The express app is ready on port ${PORT}!`);
+});
